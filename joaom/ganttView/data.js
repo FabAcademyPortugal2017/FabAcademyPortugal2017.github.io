0f76f0e72510757530741149cc7e653947476cf3
João Milheiro <joaomilheiro@gmail.com>
2017-01-30 22:54:44 +0000

    new site version upload
    week1

---
+++ joaom/ganttView/data.js
@@ -0,0 +1,49 @@
+var ganttData = [
+	{
+		id: 1, name: "Concept and planning", series: [
+			{ name: "Planned", start: new Date(2017,00,25), end: new Date(2017,01,01) },
+			{ name: "Actual", start: new Date(2017,00,25), end: new Date(2017,00,30), color: "#f0f0f0" }
+		]
+	}, 
+	{
+		id: 2, name: "Research, requirement analysis and Documentation", series: [
+			{ name: "Planned", start: new Date(2017,01,01), end: new Date(2017,01,28) },
+/*			{ name: "Actual", start: new Date(2017,01,01), end: new Date(2017,01,28), color: "#f0f0f0" },
+*/		]
+	}, 
+	{
+		id: 3, name: "Real cases study, documentation", series: [
+			{ name: "Planned", start: new Date(2017,01,20), end: new Date(2017,02,08) },
+<!--			{ name: "Actual", start: new Date(2017,01,20), end: new Date(2017,02,08), color: "#f0f0f0" }-->		
+		]
+	}, 
+	{
+		id: 4, name: "Inventory and material aquisition, documentation", series: [
+			{ name: "Planned", start: new Date(2017,02,08), end: new Date(2017,02,20) },
+/*			{ name: "Actual", start: new Date(2017,02,08), end: new Date(2017,02,20), color: "#f0f0f0" }
+*/		]
+	},
+	{
+		id: 5, name: "Electronics design, software development, programming, design, documentation", series: [
+			{ name: "Planned", start: new Date(2017,02,20), end: new Date(2017,04,15) },
+/*			{ name: "Actual", start: new Date(2017,02,20), end: new Date(2017,04,15), color: "#f0f0f0" }
+*/		]
+	}, 
+	{
+		id: 6, name: "Machining, assembling, documentation", series: [
+			{ name: "Planned", start: new Date(2017,04,15), end: new Date(2017,05,10) },
+<!--			{ name: "Actual", start: new Date(2017,00,06), end: new Date(2017,00,17), color: "#f0f0f0" }, -->		
+		]
+	}, 
+	{
+		id: 7, name: "Prototyping, machining, tests, documentation", series: [
+			{ name: "Planned", start: new Date(2017,05,1), end: new Date(2017,06,5) }
+		]
+	}, 
+	{
+		id: 8, name: "Final presentation preparation", series: [
+			{ name: "Planned", start: new Date(2017,06,5), end: new Date(2017,06,14) },
+<!--			{ name: "Actual", start: new Date(2017,01,01), end: new Date(2017,01,05), color: "#f0f0f0" }-->
+		]
+	}
+];
