aef7b21bf5c73c8e035fdff65553af786d5b869b
Lucio <git@xinfab.com>
2017-01-31 17:32:34 +0000

    update gantt

--- lucio/gantt.js
+++ lucio/gantt.js
@@ -9,11 +9,13 @@ var ganttData = [
             
             { name: "Make frame and parts", start: new Date(2017,01,08), end: new Date(2017,02,12), color: "#f0f0f0" },
 			
-            { name: "Design Electronics", start: new Date(2017,01,01), end: new Date(2017,01,07), color: "#f0f0f0" },
+            { name: "Design Electronics", start: new Date(2017,02,01), end: new Date(2017,02,15), color: "#f0f0f0" },
             
-            { name: "Make Electronics", start: new Date(2017,02,01), end: new Date(2017,03,07), color: "#f0f0f0" },
+            { name: "Make Electronics", start: new Date(2017,02,16), end: new Date(2017,03,07), color: "#f0f0f0" },
             
-            { name: "Program electronics", start: new Date(2017,03,25), end: new Date(2017,05,13), color: "#f0f0f0" },
+            { name: "Program electronics", start: new Date(2017,03,08), end: new Date(2017,04,13), color: "#f0f0f0" },
+            
+            { name: "Assembly", start: new Date(2017,04,14), end: new Date(2017,05,13), color: "#f0f0f0" },
             
             { name: "Project presentation", start: new Date(2017,05,14), end: new Date(2017,05,21), color: "#f0f0f0" },
             
