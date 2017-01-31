c4a29b01b2d54e2a3721e81eee5d1d16468bf6d4
Lucio <git@xinfab.com>
2017-01-31 16:23:37 +0000

    gantt

--- lucio/gantt.js
+++ lucio/gantt.js
@@ -1,51 +1,23 @@
 var ganttData = [
 	{
-		id: 1, name: "Gather Data", series: [
-			{ name: "Planned", start: new Date(2017,00,01), end: new Date(2017,00,03) },
-			{ name: "Actual", start: new Date(2017,00,02), end: new Date(2017,00,05), color: "#f0f0f0" }
+		id: 1, name: "Fab Academy", series: [
+			{ name: "Course duration", start: new Date(2017,00,25), end: new Date(2017,05,21) },
+			
+            { name: "Skecth", start: new Date(2017,00,25), end: new Date(2017,00,31), color: "#f0f0f0" },
+            
+            { name: "Initial Design", start: new Date(2017,01,01), end: new Date(2017,01,07), color: "#f0f0f0" },
+            
+            { name: "Make frame and parts", start: new Date(2017,01,08), end: new Date(2017,02,12), color: "#f0f0f0" },
+			
+            { name: "Design Electronics", start: new Date(2017,01,01), end: new Date(2017,01,07), color: "#f0f0f0" },
+            
+            { name: "Make Electronics", start: new Date(2017,02,01), end: new Date(2017,03,07), color: "#f0f0f0" },
+            
+            { name: "Program electronics", start: new Date(2017,03,25), end: new Date(2017,05,13), color: "#f0f0f0" },
+            
+            { name: "Project presentation", start: new Date(2017,05,14), end: new Date(2017,05,21), color: "#f0f0f0" },
+            
 		]
 	}, 
-	{
-		id: 2, name: "Feature 2", series: [
-			{ name: "Planned", start: new Date(2017,00,05), end: new Date(2017,00,20) },
-			{ name: "Actual", start: new Date(2017,00,06), end: new Date(2017,00,17), color: "#f0f0f0" },
-			{ name: "Projected", start: new Date(2017,00,06), end: new Date(2017,00,17), color: "#e0e0e0" }
-		]
-	}, 
-	{
-		id: 3, name: "Feature 3", series: [
-			{ name: "Planned", start: new Date(2017,00,11), end: new Date(2017,01,03) },
-			{ name: "Actual", start: new Date(2017,00,15), end: new Date(2017,01,03), color: "#f0f0f0" }
-		]
-	}, 
-	{
-		id: 4, name: "Feature 4", series: [
-			{ name: "Planned", start: new Date(2017,01,01), end: new Date(2017,01,03) },
-			{ name: "Actual", start: new Date(2017,01,01), end: new Date(2017,01,05), color: "#f0f0f0" }
-		]
-	},
-	{
-		id: 5, name: "Feature 5", series: [
-			{ name: "Planned", start: new Date(2017,02,01), end: new Date(2017,03,20) },
-			{ name: "Actual", start: new Date(2017,02,01), end: new Date(2017,03,26), color: "#f0f0f0" }
-		]
-	}, 
-	{
-		id: 6, name: "Feature 6", series: [
-			{ name: "Planned", start: new Date(2017,00,05), end: new Date(2017,00,20) },
-			{ name: "Actual", start: new Date(2017,00,06), end: new Date(2017,00,17), color: "#f0f0f0" },
-			{ name: "Projected", start: new Date(2017,00,06), end: new Date(2017,00,20), color: "#e0e0e0" }
-		]
-	}, 
-	{
-		id: 7, name: "Feature 7", series: [
-			{ name: "Planned", start: new Date(2017,00,11), end: new Date(2017,01,03) }
-		]
-	}, 
-	{
-		id: 8, name: "Feature 8", series: [
-			{ name: "Planned", start: new Date(2017,01,01), end: new Date(2017,01,03) },
-			{ name: "Actual", start: new Date(2017,01,01), end: new Date(2017,01,05), color: "#f0f0f0" }
-		]
-	}
+	
 ];
