4e82b1223d20035af2e62aab5c5da37c5be25ef0
ToniBarreiros <tonibarreiros@gmail.com>
2017-02-06 19:06:14 +0000

    Toni Week 2 upload

---
+++ toni/jquery.ganttView-master/example/data.js
@@ -0,0 +1,99 @@
+var ganttData = [
+	{
+		id: 1, name: "The Idea - Automated Cat Feeder ", series: [
+			{ name: "Planned", start: new Date(2017,01,25), end: new Date(2017,02,01) },
+			{ name: "Actual", start: new Date(2017,01,25), end: new Date(2017,02,01), color: "#f0f0f0" }
+		]
+	}, 
+	{
+		id: 2, name: "Project Design, 3D prototype", series: [
+			{ name: "Planned", start: new Date(2017,02,01), end: new Date(2070,02,06) },
+			{ name: "Actual", start: new Date(2017,02,01), end: new Date(2017,02,06), color: "#f0f0f0" },
+		]
+	}, 
+	{
+		id: 3, name: "Build a Small Prototype of the Project", series: [
+			{ name: "Planned", start: new Date(2017,02,06), end: new Date(2017,02,13) },
+			{ name: "Actual", start: new Date(2017,02,06), end: new Date(2017,02,13), color: "#f0f0f0" }
+		]
+	}, 
+	{
+		id: 4, name: "Electronics Production", series: [
+			{ name: "Planned", start: new Date(2017,02,13), end: new Date(2017,02,13) },
+			{ name: "Actual", start: new Date(2017,02,20), end: new Date(2017,02,20), color: "#f0f0f0" }
+		]
+	},
+	{
+		id: 5, name: "3D printing project parts", series: [
+			{ name: "Planned", start: new Date(2017,02,22), end: new Date(2017,03,01) },
+			{ name: "Actual", start: new Date(2017,02,22), end: new Date(2017,03,01), color: "#f0f0f0" }
+		]
+	}, 
+	{
+		id: 6, name: "Electronics design", series: [
+			{ name: "Planned", start: new Date(2017,03,06), end: new Date(2017,03,13) },
+			{ name: "Actual", start: new Date(2017,03,06), end: new Date(2017,03,13), color: "#f0f0f0" },
+			
+		]
+	}, 
+	{
+		id: 7, name: "Project Programming ", series: [
+			{ name: "Planned", start: new Date(2017,03,15), end: new Date(2017,03,20) },
+			{ name: "Actual", start: new Date(2017,03,15), end: new Date(2017,03,20), color: "#f0f0f0" },
+		]
+	}, 
+	{
+		id: 8, name: "Design the mechanics of the project", series: [
+			{ name: "Planned", start: new Date(2017,03,29), end: new Date(2017,04,03) },
+			{ name: "Actual", start: new Date(2017,03,29), end: new Date(2017,04,03), color: "#f0f0f0" }
+		]
+	},
+	
+		{
+		id: 9, name: " Input devices ", series: [
+			{ name: "Planned", start: new Date(2017,04,05), end: new Date(2017,04,17) },
+			{ name: "Actual", start: new Date(2017,04,05), end: new Date(2017,04,17), color: "#f0f0f0" },
+		]
+	},
+	
+	{
+		{
+		id: 10, name: "Molding parts", series: [
+			{ name: "Planned", start: new Date(2017,04,19), end: new Date(2017,04,24) },
+			{ name: "Actual", start: new Date(2017,04,19), end: new Date(2017,04,24), color: "#f0f0f0" }
+		]
+	},
+	
+	{
+		{
+		id: 11, name: "output devices", series: [
+			{ name: "Planned", start: new Date(2017,04,26), end: new Date(2017,05,01) },
+			{ name: "Actual", start: new Date(2017,04,26), end: new Date(2017,05,01), color: "#f0f0f0" }
+		]
+	},
+	
+	{
+		{
+		id: 12, name: "Build the structure of the project", series: [
+			{ name: "Planned", start: new Date(2017,05,03), end: new Date(2017,05,08) },
+			{ name: "Actual", start: new Date(2017,05,03), end: new Date(2017,05,08), color: "#f0f0f0" }
+		]
+	},
+	
+	{
+		{
+		id: 13, name: "interface and application programming", series: [
+			{ name: "Planned", start: new Date(2017,05,03), end: new Date(2017,05,24) },
+			{ name: "Actual", start: new Date(2017,05,03), end: new Date(2017,05,24), color: "#f0f0f0" }
+		]
+	
+	},
+	
+	{
+		{
+		id: 14, name: "project presentations", series: [
+			{ name: "Planned", start: new Date(2017,05,24), end: new Date(2017,06,14) },
+			{ name: "Actual", start: new Date(2017,05,24), end: new Date(2017,06,14), color: "#f0f0f0" }
+		]
+	
+];
