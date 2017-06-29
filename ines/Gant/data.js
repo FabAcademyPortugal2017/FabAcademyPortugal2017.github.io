764f011d5728deb7315a35d126f6d129cc21d08e
InesCarmo <ines.carmo.16@gmail.com>
2017-06-29 10:28:07 +0100

    ines update

--- ines/Gant/data.js
+++ ines/Gant/data.js
@@ -1,51 +1,57 @@
 var ganttData = [
 	{
-		id: 1, name: "Concept criation", series: [
+		id: 1, name: "Inicial Planing", series: [
 			{ name: "Planned", start: new Date(2017,00,25), end: new Date(2017,01,02) },
-			{ name: "Actual", start: new Date(2017,01,01), end: new Date(2017,01,08), color: "#f0f0f0" }
 		]
 	}, 
 	{
-		id: 2, name: "Research and development", series: [
-			{ name: "Planned", start: new Date(2017,01,01), end: new Date(2017,01,15) },
-			{ name: "Actual", start: new Date(2017,01,03), end: new Date(2017,01,15), color: "#f0f0f0" },
-			{ name: "Projected", start: new Date(2017,01,06), end: new Date(2017,01,17), color: "#e0e0e0" }
+		id: 2, name: "Research and Development", series: [
+			{ name: "Planned", start: new Date(2017,00,27), end: new Date(2017,01,15) },
 		]
 	}, 
 	{
-		id: 3, name: "Design", series: [
-			{ name: "Planned", start: new Date(2017,01,11), end: new Date(2017,02,03) },
-			{ name: "Actual", start: new Date(2017,01,15), end: new Date(2017,02,03), color: "#f0f0f0" }
+		id: 3, name: "Concept Generation", series: [
+			{ name: "Planned", start: new Date(2017,01,02), end: new Date(2017,02,12) },
 		]
 	}, 
 	{
-		id: 4, name: "Fabrication Method", series: [
-			{ name: "Planned", start: new Date(2017,02,01), end: new Date(2017,03,03) },
-			{ name: "Actual", start: new Date(2017,02,01), end: new Date(2017,03,05), color: "#f0f0f0" }
+		id: 4, name: "Narrow Concepts", series: [
+			{ name: "Planned", start: new Date(2017,01,20), end: new Date(2017,03,9) },
 		]
 	},
 	{
-		id: 5, name: "Feature 5", series: [
-			{ name: "Planned", start: new Date(2017,03,01), end: new Date(2017,03,20) },
-			{ name: "Actual", start: new Date(2017,03,01), end: new Date(2017,03,26), color: "#f0f0f0" }
+		id: 5, name: "Get Parts", series: [
+			{ name: "Planned", start: new Date(2017,02,26), end: new Date(2017,03,26) },
 		]
 	}, 
 	{
-		id: 6, name: "Feature 6", series: [
-			{ name: "Planned", start: new Date(2017,04,05), end: new Date(2017,04,20) },
-			{ name: "Actual", start: new Date(2017,04,06), end: new Date(2017,04,17), color: "#f0f0f0" },
-			{ name: "Projected", start: new Date(2017,04,06), end: new Date(2017,04,20), color: "#e0e0e0" }
+		id: 6, name: "Design Parts", series: [
+			{ name: "Planned", start: new Date(2017,03,19), end: new Date(2017,04,7) },
 		]
 	}, 
 	{
-		id: 7, name: "Feature 7", series: [
-			{ name: "Planned", start: new Date(2017,04,11), end: new Date(2017,05,03) }
+		id: 7, name: "Build Parts", series: [
+			{ name: "Planned", start: new Date(2017,03,25), end: new Date(2017,05,03) }
 		]
 	}, 
 	{
-		id: 8, name: "Feature 8", series: [
+		id: 8, name: "Design Electronics", series: [
+			{ name: "Planned", start: new Date(2017,03,26), end: new Date(2017,04,24) },
+		]
+		}, 
+	{
+		id: 9, name: "Build Electronics", series: [
 			{ name: "Planned", start: new Date(2017,04,01), end: new Date(2017,05,03) },
-			{ name: "Actual", start: new Date(2017,04,01), end: new Date(2017,05,05), color: "#f0f0f0" }
+		]
+		}, 
+	{
+		id: 10, name: "Testing", series: [
+			{ name: "Planned", start: new Date(2017,04,01), end: new Date(2017,05,10) },
+		]
+		}, 
+	{
+		id: 11, name: "Final Presentation", series: [
+			{ name: "Planned", start: new Date(2017,05,07), end: new Date(2017,05,21) },
 		]
 	}
 ];
